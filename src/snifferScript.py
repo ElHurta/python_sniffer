2c9c1f8b5dfcb49b4a05a1d9c54f021962f19180
ElHurta <juan.david.hurtado08@gmail.com>
2021-12-15 22:37:49 -0500

    Complete Commit

--- src/snifferScript.py
+++ src/snifferScript.py
@@ -1,9 +1,17 @@
 from scapy import *
 from scapy.all import sniff
+import pyx
 
 # Captura Sencilla de Datos:
-simpleCapture = sniff(count=3);
+simpleCapture = sniff(count=3, filter="tcp")
+
+# Exportar Datos de los Paquetes como PDF
+simpleCapture.pdfdump("pruebaExp.pdf")
 
 # Mostrar Resumen de Datos:
 simpleCapture.summary()
+
+# Mostrar Información Completa de los paquetes:
+simpleCapture[0].show()
+
 # capture = sniff(count=5, filter="tcp", prn=lambda x: x.show())
